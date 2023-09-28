8c440222eafc01082541e475da33f61116d62402
Leslie Geronimo <leslie.geronimo1402@ciencias.unam.mx>
2023-09-27 23:38:54 -0600

    Diseño Factory

--- practicas/practica3/src/Economico.java
+++ practicas/practica3/src/Economico.java
@@ -3,20 +3,34 @@
  * @author Yael Lozano Estrada - 319007095, Leslie Geronimo Soto - 320032848
  */
 
+ /**
+ * Esta línea define la clase Economico y especifica que extiende la clase Auto. Esto significa que Economico 
+ * hereda propiedades y métodos de la clase Auto y puede agregar o sobrescribir comportamientos según sea necesario.
+ */
 public class Economico extends Auto {
+    /*
+     * Este es el constructor de la clase Economico, que se llama cuando se crea una instancia de Economico.
+     */
     public Economico() {
-        nombre = "Economico";
-        llantas = "Simple";
-        motor = "Diesel";
-        carroceria = "Casual";
-        armas = "Arpones";
-        blindaje = "Simple";
+        nombre = "Economico"; /** Establece el atributo nombre de la instancia de Economico en "Economico". */
+        llantas = "Simple"; /* Establece el atributo llantas en "Simple". */
+        motor = "Diesel"; /* Establece el atributo motor en "Diesel". */
+        carroceria = "Casual"; /* Establece el atributo carroceria en "Casual". */
+        armas = "Arpones"; /* Establece el atributo armas en "Arpones". */
+        blindaje = "Simple"; /* Establece el atributo blindaje en "Simple". */
+        
+         /**
+         * Calcula el valor de cada atributo  como la suma de los valores indicados.
+         */
         ataque = 100 + 180 + 80 + 80 + 100;
         defensa = 50 + 50 + 60 + 80 + 50;
         velocidad = 20 + 100 + 30 + 40 + 40;
         costo = 1000 + 1000 + 1000 + 1500;
     }
 
+    /**
+     * Imprime las características del objeto Economico.
+     */
     @Override
     public void mostrarCaracteristicas() {
         System.out.println(nombre + ": Nissan Versa.");
