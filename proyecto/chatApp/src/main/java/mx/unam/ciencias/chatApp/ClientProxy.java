3515f5bd63d557e29531a9ac2deccdee518262a4
Yael Lozano Estrada <yaellozanoes@ciencias.unam.mx>
2023-11-09 00:34:02 -0600

    Proyecto. Conexión HttpServer.

--- proyecto/chatApp/src/main/java/mx/unam/ciencias/chatApp/ClientProxy.java
+++ proyecto/chatApp/src/main/java/mx/unam/ciencias/chatApp/ClientProxy.java
@@ -20,55 +20,126 @@ import java.lang.StringBuilder;
 public class ClientProxy {
     ArrayList<JsonObject> messages = new ArrayList<>();
     final String loginPath = "src/main/web/index.html";
-    final String appPath = "../../../../../web/app.html";
+    final String appPath = "src/main/web/app.html";
     static Client client;
 
     public static void main(String[] args) throws IOException, InterruptedException, NullPointerException {
+        int port = 8080;
         ClientProxy cp = new ClientProxy();
         client = new Client();
-        HttpServer server = cp.launchServer(8080);
-        Scanner sc = new Scanner(System.in);
-        sc.nextLine();
-        cp.stopServer(server);
+        cp.openBrowser("http://localhost:" + port);
+        HttpServer server = cp.launchServer(port);
+        //cp.stopServer(server);
+        System.out.println(client.getUsername());
+    }
 
-        client.setUsername("");
+    public String fileToString(File file) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        FileInputStream is = new FileInputStream(file);
+        Scanner sc = new Scanner(is);
+        while (sc.hasNextLine()) {
+            sb.append(sc.nextLine());
+            sb.append("\n");
+        }
+        is.close();
+        sc.close();
+        return sb.toString();
     }
 
     public HttpServer launchServer(int port) throws IOException, InterruptedException {
         HttpServer server = HttpServer.create();
+        String htmlLoginResponse = fileToString(new File(loginPath));
+        server.bind(new InetSocketAddress(port), 0);
+
+        server.start();
+        System.out.println("[HTTP SERVER] Running on port " + port);
+
+        server.createContext("/",
+                exchange -> {
+                    exchange.getResponseHeaders().set("Content-Type","text/html");
+                    exchange.sendResponseHeaders(200, htmlLoginResponse.getBytes().length);
+                    OutputStream os = exchange.getResponseBody();
+                    os.write(htmlLoginResponse.getBytes(StandardCharsets.UTF_8));
+                    os.flush();
+                    os.close();
+
+                    String query = exchange.getRequestURI().getQuery();
+                    if (query != null && query.contains("name"))
+                        client.setUsername(query.substring(5));
+                    System.out.println("[HTTP SERVER:8080] Query: " + query);
+                }
+        );
+        while (client.getUsername().equals("")) {
+            Thread.sleep(10);
+        }
+
+        String htmlAppResponse = fileToString(new File(appPath)).replace("{username}", client.getUsername());
+        server.removeContext("/");
+        server.createContext("/",
+                exchange -> {
+                    exchange.getResponseHeaders().set("Content-Type","text/html");
+                    exchange.sendResponseHeaders(200, htmlAppResponse.getBytes().length);
+                    OutputStream os = exchange.getResponseBody();
+                    os.write(htmlAppResponse.getBytes(StandardCharsets.UTF_8));
+                    os.flush();
+                    os.close();
+
+                    String query = exchange.getRequestURI().getQuery();
+                    if (query != null && query.contains("name"))
+                        client.setUsername(query.substring(5));
+                    System.out.println("[HTTP SERVER:8080] Query: " + query);
+                }
+        );
+        return server;
+    }
+/**
+    public HttpServer launchAppServer(int port) throws IOException, InterruptedException {
+        HttpServer server = HttpServer.create();
         StringBuilder sb = new StringBuilder();
-        FileInputStream is = new FileInputStream("src/main/web/index.html");
+        FileInputStream is = new FileInputStream("src/main/web/app.html");
         Scanner sc = new Scanner(is);
         while (sc.hasNextLine()) {
-            sb.append(sc.nextLine().trim());
+            sb.append(sc.nextLine());
+            sb.append("\n");
         }
         is.close();
         String htmlResponse = sb.toString();
-        //String htmlResponse = "asd";
-        System.out.println(htmlResponse);
         server.bind(new InetSocketAddress(port), 0);
 
         server.start();
 
         server.createContext("/",
                 exchange -> {
-                    exchange.getResponseHeaders().set("Content-Type","text/html");
+                    exchange.getResponseHeaders().set("Content-Type", "text/html");
                     exchange.sendResponseHeaders(200, htmlResponse.getBytes().length);
                     OutputStream os = exchange.getResponseBody();
                     os.write(htmlResponse.getBytes(StandardCharsets.UTF_8));
                     os.flush();
                     os.close();
+
+                    String query = exchange.getRequestURI().getQuery();
+                    System.out.println("[HTTP SERVER:8080] Query: " + query);
                 }
         );
-
-
-
-        System.out.println("a");
+        System.out.println("[HTTP SERVER] Running on port " + port);
         return server;
     }
-
+*/
     public void stopServer(HttpServer server) {
         server.stop(1);
+        System.out.println("[HTTP SERVER] Stoping Server.");
+    }
+
+    public void openBrowser(String url) throws IOException{
+        String osName = System.getProperty("os.name");
+        if (osName.contains("Windows"))
+            Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + url);
+        else if (osName.contains("Linux"))
+            Runtime.getRuntime().exec("xdg-open " + url);
+        else if (osName.contains("Mac OS X"))
+            Runtime.getRuntime().exec("open " + url);
+        else
+            System.out.println("Ingrese a la siguiente URL: " + url);
     }
 
     public static void printErrorMessage(JsonObject error) {
